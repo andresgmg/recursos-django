60c00f2511c32ff9da3ad73a405b878221bf5a00
Andres Marquez <72410509+andresgmg@users.noreply.github.com>
2023-12-05 15:37:45 -0300

    lecciones 8,9 y 10

--- tutorial-django/tutorial/urls.py
+++ tutorial-django/tutorial/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import home, post
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("blog/", home),
+    path("blog/<id>", post),
 ]
